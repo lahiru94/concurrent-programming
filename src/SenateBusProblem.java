5e3c92780aa93a3fc47b06a40ceff0370646f77f
Lahiru de Alwis <lahiru@zeptolytics.com>
2018-02-09 19:06:15 +0530

    bus and rider creators added.TODO:time intervals in normal distribution

--- src/SenateBusProblem.java
+++ src/SenateBusProblem.java
@@ -1,14 +1,17 @@
 import java.util.concurrent.Semaphore;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class SenateBusProblem {
+    int fast_forward = 100;
     int riders_waiting = 0;                 //count of riders in the boarding area
     Semaphore mutex = new Semaphore(1);     //to protect waiting riders
     Semaphore bus = new Semaphore(0);       //to signal arrival of a bus
     Semaphore boarded = new Semaphore(0);   // to signal a rider has been boarded
 
-    public static void main(String args[]) {
-        SenateBusProblem senateBusProblem = new SenateBusProblem();
-    }
+    
 
     class Bus implements Runnable {
 
@@ -53,5 +56,49 @@ public class SenateBusProblem {
 
         }
     }
+
+    class RideCreator implements Runnable{
+        int rider_id=0;
+        int mean_rider=30000/fast_forward;
+        @Override
+        public void run(){
+            while(true){
+                try {
+                    Rider new_rider = new Rider();
+                    new Thread(new_rider).start();
+                    Thread.sleep(mean_rider);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(SenateBusProblem.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    class BusCreator implements Runnable{
+        int bus_id=0;
+        int mean_bus=1200000/fast_forward;
+        @Override
+        public void run(){
+            while(true){
+                try {
+                    Thread.sleep(mean_bus);
+                    Bus new_bus = new Bus();
+                    new Thread(new_bus).start();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(SenateBusProblem.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SenateBusProblem simulation = new SenateBusProblem();
+        new Thread(simulation.new BusCreator()).start();
+        new Thread(simulation.new RideCreator()).start();
+
+    }
+
+
 
 }
